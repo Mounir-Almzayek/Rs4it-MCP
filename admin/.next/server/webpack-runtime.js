95072fdb08b25bc899507aecb492827de55e4c51
Mounir Almzayek <mouneeralmzayek@gmail.com>
2026-03-08 15:02:53 +0300

    Update admin panel API routes and roles configuration. Introduce new route for handling role-specific operations and enhance type definitions for better type safety. Modify roles.json to include additional roles and inheritance structure, improving role management capabilities.

--- admin/.next/server/webpack-runtime.js
+++ admin/.next/server/webpack-runtime.js
@@ -130,7 +130,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	(() => {
-/******/ 		__webpack_require__.h = () => ("bc026c8e02c4ae6f")
+/******/ 		__webpack_require__.h = () => ("3e07c9b7141ca296")
 /******/ 	})();
 /******/ 	
 /******/ 	/* webpack/runtime/hasOwnProperty shorthand */
